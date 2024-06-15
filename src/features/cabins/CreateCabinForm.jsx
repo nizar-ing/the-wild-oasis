fc38e26f212ec300e55882665190aa2516669bae
nizarIng <nizar.ilahi@gmail.com>
2024-06-15 18:30:38 +0100

    Handling the add cabin form errors using react-hook-form

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 
 import Input from "../../ui/Input";
 import Form from "../../ui/Form";
@@ -6,93 +6,97 @@ import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import {useForm} from "react-hook-form";
-import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {createCabin} from "../../services/apiCabins.js";
 import toast from "react-hot-toast";
-
-const FormRow = styled.div`
-    display: grid;
-    align-items: center;
-    grid-template-columns: 24rem 1fr 1.2fr;
-    gap: 2.4rem;
-
-    padding: 1.2rem 0;
-
-    &:first-child {
-        padding-top: 0;
-    }
-
-    &:last-child {
-        padding-bottom: 0;
-    }
-
-    &:not(:last-child) {
-        border-bottom: 1px solid var(--color-grey-100);
-    }
-
-    &:has(button) {
-        display: flex;
-        justify-content: flex-end;
-        gap: 1.2rem;
-    }
-`;
-
-const Label = styled.label`
-    font-weight: 500;
-`;
-
-const Error = styled.span`
-    font-size: 1.4rem;
-    color: var(--color-red-700);
-`;
+import {FormRow} from "../../ui/FormRow.jsx";
 
 function CreateCabinForm() {
-    const {register, handleSubmit, reset} = useForm();
+    const {register, handleSubmit, reset, formState, getValues} = useForm();
+    console.log(getValues());
     const queryClient = useQueryClient();
     const {mutate, isLoading: isCreating} = useMutation({
         mutationFn: createCabin, //  <==> (newCabin) => createCabin(newCabin)
-        onSuccess: () => {
+        onSuccess: async () => {
             toast.success("New cabin successfully created");
-            queryClient.invalidateQueries({
+            await queryClient.invalidateQueries({
                 queryKey: ['cabins']
             });
             reset();
         },
         onError: error => toast.error(error.message)
     });
+    const {errors} = formState;
+
     const onSubmit = (data) => {
         mutate(data);
     }
+    const onError = (errors) => {
+        //console.log(errors); we can not use the "onError" method since we have this from the useForm hook. Perhaps we can use it for centric stuffs
+    }
+
     return (
-        <Form onSubmit={handleSubmit(onSubmit)}>
-            <FormRow>
-                <Label htmlFor="name">Cabin name</Label>
-                <Input type="text" id="name" {...register('name')} />
+        <Form onSubmit={handleSubmit(onSubmit, /*onError*/)}>
+            <FormRow label="Cabin name" error={errors?.name?.message}>
+                <Input
+                    type="text"
+                    id="name"
+                    {...register('name',
+                        {required: 'This field is required'}
+                    )}
+                />
             </FormRow>
 
-            <FormRow>
-                <Label htmlFor="maxCapacity">Maximum capacity</Label>
-                <Input type="number" id="maxCapacity" {...register('maxCapacity')} />
+            <FormRow label="Maximum capacity" error={errors.maxCapacity?.message}>
+                <Input
+                    type="number"
+                    id="maxCapacity"
+                    {...register("maxCapacity",
+                        {
+                                required: 'This field is required',
+                                min: {value: 1, message: 'capacity should be at least 1'}
+                               }
+                    )}
+                />
             </FormRow>
 
-            <FormRow>
-                <Label htmlFor="regularPrice">Regular price</Label>
-                <Input type="number" id="regularPrice" {...register('regularPrice')} />
+            <FormRow label="Regular price" error={errors.regularPrice?.message}>
+                <Input
+                    type="number"
+                    id="regularPrice"
+                    desabled={isCreating}
+                    {...register('regularPrice', {
+                        required: 'This field is required',
+                        min: {value: 1, message: 'capacity should be at least 1'}
+                    })}
+                />
             </FormRow>
 
-            <FormRow>
-                <Label htmlFor="discount">Discount</Label>
-                <Input type="number" id="discount" defaultValue={0} {...register('discount')} />
+            <FormRow label="Discount" error={errors.discount?.message}>
+                <Input
+                    type="number"
+                    id="discount"
+                    desabled={isCreating}
+                    defaultValue={0}
+                    {...register('discount',
+                        {required: 'This field is required',
+                                validate: (value) => Number(value) <= Number(getValues().regularPrice) || "the discount should be less than the regular price"
+                               }
+                    )
+                    }
+                />
             </FormRow>
 
-            <FormRow>
-                <Label htmlFor="description">Description for website</Label>
-                <Textarea type="number" id="description" defaultValue="" {...register('description')} />
+            <FormRow label="Description for website" error={errors.description?.message}>
+                <Textarea type="number"
+                          id="description"
+                          defaultValue=""
+                          desabled={isCreating}
+                          {...register('description', {required: 'This field is required'})}
+                />
             </FormRow>
 
-            <FormRow>
-                <Label htmlFor="image">Cabin photo</Label>
-                <FileInput id="image" accept="image/*"/>
+            <FormRow label="Cabin photo">
+                <FileInput id="image" accept="image/*" disabled={isCreating}/>
             </FormRow>
 
             <FormRow>
@@ -100,7 +104,7 @@ function CreateCabinForm() {
                 <Button variation="secondary" type="reset">
                     Cancel
                 </Button>
-                <Button>Add cabin</Button>
+                <Button disabled={isCreating}>Add cabin</Button>
             </FormRow>
         </Form>
     );
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -16,6 +16,7 @@ const Main = styled.main`
     //box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;
     //box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
     box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
+    overflow: scroll;
 `;
 
 const Container = styled.div`

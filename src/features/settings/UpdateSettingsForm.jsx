86c45ad9d7c70d5cb09044133b34922e75349d11
nizarIng <nizar.ilahi@gmail.com>
2024-06-18 00:31:06 +0100

    Fetching Application settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,74 +1,56 @@
-import Spinner from 'ui/Spinner';
-import { useSettings } from 'features/settings/useSettings';
-import { useUpdateSetting } from 'features/settings/useUpdateSetting';
-
-import Form from 'ui/Form';
-import FormRow from 'ui/FormRow';
-import Input from 'ui/Input';
+import Form from "../../ui/Form.jsx";
+import {FormRow} from "../../ui/FormRow.jsx";
+import Input from "../../ui/Input.jsx";
+import {useSettings} from "./useSettings.js";
+import Spinner from "../../ui/Spinner.jsx";
 
 function UpdateSettingsForm() {
-  const {
-    settings: {
-      minBookingLength,
-      maxBookingLength,
-      maxGuestsPerBooking,
-      breakfastPrice,
-    } = {},
-    isLoading,
-  } = useSettings();
-  const { mutate: updateSetting, isLoading: isUpdating } = useUpdateSetting();
+    const {
+        isLoading,
+        settings: {
+            minBookingLength,
+            maxBookingLength,
+            maxGuestsPerBooking,
+            breakfastPrice
+        } = {}
+    } = useSettings();
 
-  // return <Spinner />;
-  if (isLoading) return <Spinner />;
+    // return <Spinner />;
+    if (isLoading) return <Spinner />;
 
-  function handleBlur(e, field) {
-    const { value } = e.target;
-
-    if (!value) return;
-    updateSetting({ [field]: value });
-  }
-
-  // This time we are using UNCONTROLLED fields, so we will NOT store state
-  return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input
-          type='number'
-          defaultValue={minBookingLength}
-          onBlur={(e) => handleBlur(e, 'minBookingLength')}
-          disabled={isUpdating}
-          id='min-nights'
-        />
-      </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input
-          type='number'
-          defaultValue={maxBookingLength}
-          onBlur={(e) => handleBlur(e, 'maxBookingLength')}
-          disabled={isUpdating}
-          id='max-nights'
-        />
-      </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input
-          type='number'
-          defaultValue={maxGuestsPerBooking}
-          onBlur={(e) => handleBlur(e, 'maxGuestsPerBooking')}
-          disabled={isUpdating}
-          id='max-guests'
-        />
-      </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input
-          type='number'
-          defaultValue={breakfastPrice}
-          onBlur={(e) => handleBlur(e, 'breakfastPrice')}
-          disabled={isUpdating}
-          id='breakfast-price'
-        />
-      </FormRow>
-    </Form>
-  );
+    // This time we are using UNCONTROLLED fields, so we will NOT store state
+    return (
+        <Form>
+            <FormRow label='Minimum nights/booking'>
+                <Input
+                    type='number'
+                    id='min-nights'
+                    defaultValue={minBookingLength}
+                />
+            </FormRow>
+            <FormRow label='Maximum nights/booking'>
+                <Input
+                    type='number'
+                    id='max-nights'
+                    defaultValue={maxBookingLength}
+                />
+            </FormRow>
+            <FormRow label='Maximum guests/booking'>
+                <Input
+                    type='number'
+                    id='max-guests'
+                    defaultValue={maxGuestsPerBooking}
+                />
+            </FormRow>
+            <FormRow label='Breakfast price'>
+                <Input
+                    type='number'
+                    id='breakfast-price'
+                    defaultValue={breakfastPrice}
+                />
+            </FormRow>
+        </Form>
+    );
 }
 
 export default UpdateSettingsForm;

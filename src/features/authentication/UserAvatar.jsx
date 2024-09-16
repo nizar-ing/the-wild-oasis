34689d47f715ca143d58fe29bc785cbaaa7f0785
nizarIng <nizar.ilahi@gmail.com>
2024-09-16 23:24:15 +0100

    Building the app header.

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useUser } from "./useUser";
 
 const StyledUserAvatar = styled.div`
   display: flex;
@@ -11,11 +12,27 @@ const StyledUserAvatar = styled.div`
 
 const Avatar = styled.img`
   display: block;
-  width: 4rem;
-  width: 3.6rem;
+  width: 3.4rem;
   aspect-ratio: 1;
   object-fit: cover;
   object-position: center;
   border-radius: 50%;
   outline: 2px solid var(--color-grey-100);
 `;
+
+function UserAvatar() {
+    const { user } = useUser();
+    const { fullName, avatar } = user.user_metadata;
+
+    return (
+        <StyledUserAvatar>
+            <Avatar
+                src={avatar || "default-user.jpg"}
+                alt={`Avatar of ${fullName}`}
+            />
+            <span>{fullName}</span>
+        </StyledUserAvatar>
+    );
+}
+
+export default UserAvatar;
